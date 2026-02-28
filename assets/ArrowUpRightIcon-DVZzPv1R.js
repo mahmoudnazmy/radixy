dd2b0ed28ca46aaff273e9b9b5007612e0d27ea1
mahmoud nazmy <youssef200nazmy2@gmail.com>
2026-03-01 00:51:27 +0200

    Initial commit: Radiology educational website

---
+++ assets/ArrowUpRightIcon-DVZzPv1R.js
@@ -0,0 +1,1 @@
+import{o,b as r,e}from"./vue-vendor-DnPaZoA8.js";function t(t,n){return o(),r("svg",{xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24","stroke-width":"1.5",stroke:"currentColor","aria-hidden":"true","data-slot":"icon"},[e("path",{"stroke-linecap":"round","stroke-linejoin":"round",d:"m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"})])}export{t as r};
